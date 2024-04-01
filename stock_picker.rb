a0ba0ee9055b3bcfe8422b6e259f4098c6d7b96d
cereia <104402370+cereia@users.noreply.github.com>
2024-04-01 17:40:44 -0400

    Add for loop test and notes

--- stock_picker.rb
+++ stock_picker.rb
@@ -6,15 +6,35 @@
 # set inital buy date to 0 and sell date to 1
 # compare array[sell] - array[buy] until you have the highest difference where buy < sell
 
+# check if array[sell] < i --> set sell = i if true
+# if i < array[buy]
+# check if array[i + n] - array[i] > array[sell] - array[buy] ---> set new buy, sell if true
+# 
 
 def stock_picker(array)
   buy = 0
   sell = 1
-p array[buy]
+  if array[sell] - array[buy] < 0
+    buy = sell
+    sell += 1
+  end 
+  
+  for i in array
+    p array.find_index(i)
+    p array[array.find_index(i)]
+    # p array[i]
+    # if (array[array.find_index(i + 1)] - array[(array.find_index(i))]) > (array[sell] - array[buy])
+    #   buy = array.find_index(i)
+    #   sell = array.find_index(i + 1)  
+    # end
+  end
+
+  # p array[buy]
+  # p array[sell]
 end
 
 stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
 # [1, 4] 15 - 3 = 12
 
-stock_picker([3, 16, 8, 2, 10, 5, 12, 17, 1])
+# stock_picker([3, 16, 8, 2, 10, 5, 12, 17, 1])
 # [3, 7] 17 - 2 = 15
