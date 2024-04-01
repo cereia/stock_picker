aa604d34ebcffb4492b7c2f314380f88f7c1c388
cereia <104402370+cereia@users.noreply.github.com>
2024-04-01 17:01:15 -0400

    Add test cases and some pseudo code and variables

--- stock_picker.rb
+++ stock_picker.rb
@@ -3,3 +3,18 @@
 # days start from 0
 
 # index of buy day is always lower than index of sell day
+# set inital buy date to 0 and sell date to 1
+# compare array[sell] - array[buy] until you have the highest difference where buy < sell
+
+
+def stock_picker(array)
+  buy = 0
+  sell = 1
+p array[buy]
+end
+
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+# [1, 4] 15 - 3 = 12
+
+stock_picker([3, 16, 8, 2, 10, 5, 12, 17, 1])
+# [3, 7] 17 - 2 = 15
