a6383b14c2c7010fa1b5e05d5e11e6cc67592ef3
cereia <104402370+cereia@users.noreply.github.com>
2024-04-01 16:20:35 -0400

    Add project details to README and create ruby file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # stock_picker
+
+The goal of the project is to return an array of the best day to buy and the best day to sell given an array of stock prices.
---
+++ stock_picker.rb
@@ -0,0 +1,5 @@
+# buy has to come before sell, so you can only sell on days after you buy
+  # when buy value is determined, remove values from before buy day (index) to determine sell day
+# days start from 0
+
+# index of buy day is always lower than index of sell day
