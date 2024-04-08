44519c7f2cc91275cce557032fa6f3b096974a40
cereia <104402370+cereia@users.noreply.github.com>
2024-04-08 17:00:02 -0400

    Stock-picker returns the best days to buy/sell
    Comments were added/removed to help clarify

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,66 +1,68 @@
-# check to make sure that the min and max values are not at the ends of the array --> auto reject
-# then look for the min and max of what's left after removing the ends 
 # if the min is at an index greater than the max, split the array at that min
 # [0...min] and [min..-1]
 # with the two new arrays, find min and max of each until index of max > index of min
 # when max index > min index, save their difference in a difference variable for comparison
-# compare the differences of min-maxes among all the subarrays and save and return the highest pair
+# compare the differences of min-maxes among all the subarrays and return the highest pair
 # after getting the min max combo, look through the original array for their indices in the original
-# min doesn't matter, but look for the max index that is greater than the min index 
-# --> array.index() / array.find_index() return first match
-    # use a while loop with an if?
-    # i = 0
-    # while i < array.length do 
-    #   if array[i] == max && i > buy_index 
-    #     sell_index = i 
-    #     break
-    #   end
-    #   i += 1
-    # end
+# min's index doesn't matter, but look for the max index that is greater than the min index
 
 def stock_picker(array)
-  # min_index = find_min(array)
-  # max_index = find_max(array)
   results = {}
   find_stock_pairs(array, results)
-  
-  results
 
-  # p min_index
-  # p max_index
-
-  # p array[min_index]
-  # p array[max_index]
+  # sort the keys from results from largest to smallest and grab first one
+  key = results.keys.map { |value| value.to_i }.sort{ |a, b| b <=> a }[0].to_s
+  buy_index = array.index(results[key][0])
+  if results.length == 1 
+    # if the hash has length = 1, that means min < max, so finding the first values that match works
+    sell_index = array.index(results[key][1])
+  else
+    i = 0
+    while i < array.length do 
+      if array[i] == results[key][1] && i > buy_index
+        sell_index = i 
+        break
+      end
+      i += 1
+    end
+  end 
+  [buy_index, sell_index]
 end
 
+# HELPER FUNCTIONS
 def find_min(array)
+  # set min_index to min value of the array except when the min is the last item of the array
   min_index = array.index(array.min) != array.length - 1 ? 
     array.index(array.min) : 
+    # use a modified array where the last item is removed and find min
     array.index(array[0...array.index(array.min)].min)
 end
 
 def find_max(array)
+  # set max_index to max value of the array except when the max is the first item of the array
   max_index = array.index(array.max) != 0 ? 
     array.index(array.max) : 
+    # use a modified array where first item is removed and find max 
     array.index(array[1..-1].max)
 end
 
 def find_stock_pairs(array, hash)
+  # find index of min and max values of the array passed in
   min = find_min(array)
   max = find_max(array)
 
   if min > max 
+    # if index of min > index of max, split the array at index of the min value and test again
     sub_array1 = array[0...min]
-    # p sub_array1
     sub_array2 = array[min..-1]
-    # p sub_array2
+    # recursive call to get all subarrays and collect results in single hash
     find_stock_pairs(sub_array1, hash)
     find_stock_pairs(sub_array2, hash)
   else
+    # when index of min < index of max, find difference of the values
     difference = array[max] - array[min]
+    # make the difference a string to be used as the key for the hash
     hash_key = difference.to_s
-    # make the difference between the values the key and the values themselves the array
-    # this makes it easier to find their indices in the original array easier 
     hash[hash_key] = [array[min], array[max]]
     hash
   end
@@ -69,17 +71,17 @@ end
 
 # TEST CASES 
 
-# p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
 # [1, 4] 15 - 3 = 12
 
-# p stock_picker([3, 16, 8, 2, 10, 5, 12, 17, 1])
+p stock_picker([3, 16, 8, 2, 10, 5, 12, 17, 1])
 # [3, 7] 17 - 2 = 15
 
-# p stock_picker([43, 40, 32, 1, 29])
+p stock_picker([43, 40, 32, 1, 29])
 # [3, 4] 29 - 1 = 28
 
-# p stock_picker([230, 72, 80, 40, 49, 70, 42, 1, 33])
+p stock_picker([230, 72, 80, 40, 49, 70, 42, 1, 33])
 # [7, 8] 33 - 1 = 32
 # with a[1] = 72 and a[2] = 80, gets [1, 2]
 
-p stock_picker([42, 16, 32, 4, 34, 3, 29, 5, 1, 30])
+p stock_picker([42, 34, 16, 4, 34, 3, 29, 4, 1, 30])
