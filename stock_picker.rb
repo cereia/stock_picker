fa3ef32312adce5ec4d28d556f10bb303e7b2b9a
cereia <104402370+cereia@users.noreply.github.com>
2024-04-08 15:57:31 -0400

    Move creating subarrays into own method
    Hash has all the min-max pairs instead of multiple single value hashes

--- stock_picker.rb
+++ stock_picker.rb
@@ -15,33 +15,16 @@
     #     sell_index = i 
     #     break
     #   end
+    #   i += 1
     # end
 
 def stock_picker(array)
-  min_index = find_min(array)
-  max_index = find_max(array)
-  p min_index
-  p max_index
+  # min_index = find_min(array)
+  # max_index = find_max(array)
   results = {}
-
-  if min_index > max_index 
-    sub_array1 = array[0...min_index]
-    p sub_array1
-    sub_array2 = array[min_index..-1]
-    p sub_array2
-    stock_picker(sub_array1)
-    stock_picker(sub_array2)
-  else
-    difference = array[max_index] - array[min_index]
-    results_key = difference.to_s
-    # result = []
-    # result.push(min_index, max_index)
-    # make the difference between the values the key and the values themselves the array
-    # this makes it easier to find their indices in the original array easier 
-    results[results_key] = [array[min_index], array[max_index]]
-    p results
-  end
-  # p results.keys
+  find_stock_pairs(array, results)
+  
+  results
 
   # p min_index
   # p max_index
@@ -62,21 +45,41 @@ def find_max(array)
     array.index(array[1..-1].max)
 end
 
-# p find_min_max([230, 72, 80, 40, 49, 70, 42, 1, 33])
+def find_stock_pairs(array, hash)
+  min = find_min(array)
+  max = find_max(array)
+
+  if min > max 
+    sub_array1 = array[0...min]
+    # p sub_array1
+    sub_array2 = array[min..-1]
+    # p sub_array2
+    find_stock_pairs(sub_array1, hash)
+    find_stock_pairs(sub_array2, hash)
+  else
+    difference = array[max] - array[min]
+    hash_key = difference.to_s
+    # make the difference between the values the key and the values themselves the array
+    # this makes it easier to find their indices in the original array easier 
+    hash[hash_key] = [array[min], array[max]]
+    hash
+  end
+end
+
 
 # TEST CASES 
 
-p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+# p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
 # [1, 4] 15 - 3 = 12
 
-# stock_picker([3, 16, 8, 2, 10, 5, 12, 17, 1])
+# p stock_picker([3, 16, 8, 2, 10, 5, 12, 17, 1])
 # [3, 7] 17 - 2 = 15
 
 # p stock_picker([43, 40, 32, 1, 29])
 # [3, 4] 29 - 1 = 28
 
-#currently discards all the min values except 40 because they're all smaller than the max value
-
 # p stock_picker([230, 72, 80, 40, 49, 70, 42, 1, 33])
 # [7, 8] 33 - 1 = 32
 # with a[1] = 72 and a[2] = 80, gets [1, 2]
+
+p stock_picker([42, 16, 32, 4, 34, 3, 29, 5, 1, 30])
